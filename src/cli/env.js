46ad8efa009c2c31881f1b7979fe57c1df4bd419
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-10-26 18:28:24 +0500

    feat: done cli task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-  // Write your code here
+  const envEntries = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`);
+
+  console.log(envEntries.join("\n"));
 };
 
 parseEnv();
