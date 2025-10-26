aa565596debb554294b17909149438dcdd9be3de
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-10-26 18:31:06 +0500

    feat: done fs tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from "fs/promises";
+import path from "path";
+
 const copy = async () => {
-  // Write your code here
+  const filesPath = path.join("files");
+  const copyPath = path.join("files_copy");
+
+  try {
+    const stats = await fs.stat(filesPath);
+    if (!stats.isDirectory()) throw new Error("FS operation failed");
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fs.stat(copyPath);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code !== "ENOENT") throw new Error("FS operation failed");
+  }
+
+  try {
+    await fs.cp(filesPath, copyPath, { recursive: true });
+    console.log("Success");
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import path from "path";
+import fs from "fs/promises";
+
 const read = async () => {
-  // Write your code here
+  const filePath = path.join("files", "fileToRead.txt");
+
+  try {
+    const data = await fs.readFile(filePath);
+    console.log(data.toString());
+  } catch (error) {
+    throw new Error("Fs operation failed");
+  }
 };
 
 await read();
